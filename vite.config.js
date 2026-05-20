5cfe1cc9621531aa99f1082d96870e2c257e31b1
bvronlinedev <bvronlinedev@gmail.com>
2026-05-20 22:56:34 +0530

    custom-domain

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,23 @@ import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
-  react(),
-  tailwindcss(),
-  svgr({ 
-      svgrOptions: {
-        // svgr options
-      },
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {},
     }),
-],
+  ],
+
   resolve: {
     alias: {
-      src: "/src",
-      components: "/src/components",
-      assets: "/src/assets",
-      lib: "/src/lib",
+      src: '/src',
+      components: '/src/components',
+      assets: '/src/assets',
+      lib: '/src/lib',
     },
   },
-  base: "/"
-})
+
+  base: '/',
+}))
